949c7032f08be6d647e9bda87d656f4f273d2271
Nandira Paturohman <nandirasp@gmail.com>
2017-11-07 16:25:12 +0700

    create model

---
+++ migrations/20171107085735-create-movie.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Movies', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      genre: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      duration: {
+        type: Sequelize.INTEGER
+      },
+      ticket_price: {
+        type: Sequelize.INTEGER
+      },
+      ScheduleId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        defaultValue: new Date(),
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        defaultValue: new Date(),
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Movies');
+  }
+};
